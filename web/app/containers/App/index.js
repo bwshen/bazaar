2b04829cc2fb1416d8c8c08924eb854da5967948
Andrew Chan <andrew.chan@rubrik.com>
2018-06-13 19:04:13 -0700

    Some order info

--- web/app/containers/App/index.js
+++ web/app/containers/App/index.js
@@ -60,7 +60,6 @@ class App extends React.Component {
             <Route path="" component={NotFoundPage} />
           </Switch>
         </AppBody>
-        <Footer />
       </AppWrapper>
     );
   }
