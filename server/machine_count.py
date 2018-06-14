e85fbce47efa07394584a27b4076283d42025192
Michael Li <michael.li@rubrik.com>
2018-06-13 20:22:09 -0700

    Added counts to machines

--- server/machine_count.py
+++ server/machine_count.py
@@ -1,5 +1,44 @@
-import json
+import requests
+import server
 
-def rktest_yml_count(api_token):
+class TokenAuth:
+    def __init__(self, api_token):
+        self.token = api_token
 
+    def __call__(self, request):
+        request.headers['Authorization'] = self.token
+        return request
+
+def machine_count(api_token, machine_type):
+    print(api_token)
+    kwargs = {'auth': TokenAuth(api_token)}
+    kwargs['verify'] = False
+
+    endpoint = machine_type
+    print("Worked")
+    print(server.url().rstrip('/') + '/' + endpoint.lstrip('/'))
+    response = requests.request('GET', server.url().rstrip('/') + '/' + endpoint.lstrip('/'), **kwargs)
+
+    json = response.json()
+
+    total_free = 0
+
+    while True:
+        for machine in json['results']:
+            if machine['held_by'] is None:
+                total_free += 1
+
+        if not json['next'] is None:
+            json = requests.request('GET', json['next'], **kwargs).json()
+        else:
+            break
+
+    print(json)
+    return {'free': total_free, 'machines': json['count']}
+
+
+
+
+
+    
 
--- server/server.py
+++ server/server.py
@@ -1,6 +1,42 @@
+import yaml
+import machine_count
 from flask import Flask
+from flask import request
+from flask import jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
         return "Hello World!"
+
+@app.route("/rktest_yml_count", methods=['GET'])
+def rktest_yml_machine_count():
+    return jsonify(machine_count.machine_count(request.headers.get('authorization'), '/rktest_ymls'))
+
+@app.route("/ubuntu_machines_count", methods=['GET'])
+def ubuntu_machine_count():
+    return jsonify(machine_count.machine_count(request.headers.get('authorization'), '/ubuntu_machines/'))
+
+@app.route("/esx_hosts_count", methods=['GET'])
+def esx_hosts_count():
+    return jsonify(machine_count.machine_count(request.headers.get('authorization'), '/esx_hosts/'))
+
+@app.route("/sd_dev_machines_count", methods=['GET'])
+def sd_dev_machines_count():
+    return jsonify(machine_count.machine_count(request.headers.get('authorization'), '/sd_dev_machines/'))
+
+@app.route("/mssql_servers_count", methods=['GET'])
+def mssql_servers_count():
+    return jsonify(machine_count.machine_count(request.headers.get('authorization'), '/mssql_servers/'))
+
+
+
+def url():
+    try:
+        with open('server_config.yml', 'r') as server_profile:
+            server_info = yaml.safe_load(server_profile)
+        url = server_info['url']
+    except Exception:
+        raise Exception("Cannot find server config file")
+
+    return url
