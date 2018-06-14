9ef7e072e84205cfce3cab06260eea0ef3e967df
Thomas Chang <thomas.chang@rubrik.com>
2018-06-13 22:34:33 -0700

    Fixed bodega_api.py

--- lab/bin/bodega_api.py
+++ lab/bin/bodega_api.py
@@ -2,7 +2,7 @@
 
 
 from flask import Flask
-import bodega-orders
+
 from bodega_order import get_order_times
 
 app = Flask(__name__)
@@ -11,10 +11,10 @@ app = Flask(__name__)
 def hello():
     return "Hello World 2!"
 
-@app.route("/order_times")
+@app.route("/order_times/<sid>")
 def get_list_orders(sid):
   (order_time, target_time) = get_order_times(sid)
-  print('order_time:'+str(order_time)+',target_time:'+str(target_time))
+  return "{'order_time':"+str(order_time)+",'target_time':"+str(target_time)+"}"
 
 
   
